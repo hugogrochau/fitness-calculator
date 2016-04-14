78ae1b30342146d345e26bcd06673543bb5f4003
Hugo Grochau <hugogrochau@yahoo.com>
2016-04-14 22:03:52 +0000

    Add initial, statistics and results menus

---
+++ menu.lua
@@ -0,0 +1,67 @@
+-- Define module menu
+local menu = {}
+
+-- Import external modules
+local data = require("data")
+
+-- Receives a title and prints a header
+local function header(title)
+	io.write('******************\n')
+	io.write('**     ' .. title .. '     **\n')
+	io.write('******************\n')
+	io.write('\n')
+end
+
+
+-- Getter initial data from user like name, age, gender and height
+function menu.initial()
+    local initialInfo = {}
+    header('INITIAL')
+    
+    io.write('What\'s your name? ')
+    initialInfo['name'] = io.read()
+    
+    io.write('What\'s your age? ')
+    initialInfo['age'] = tonumber(io.read())
+    
+    io.write('What\'s your gender (m/f)?')
+    initialInfo['gender'] = string.lower(io.read())
+    
+    io.write('What\'s your height in centimeters? ')
+    initialInfo['height'] = tonumber(io.read())
+    
+    return initialInfo
+end
+
+-- Get changeable data from user, like weight and waist
+function menu.statistics()
+    local result = {}
+    header('STATISTICS')
+    io.write('What\'s your weight in kilograms? ')
+    result['weight'] = tonumber(io.read())
+    
+    io.write('What\'s your waist circumference in centimeters? ')
+    result['waist'] = tonumber(io.read())
+    return result
+end
+
+-- Print all results to the screen
+function menu.results(bmi, bmr, idealBmi, bodyFat)
+    -- Print body mass index
+    print('---- BODY MASS INDEX ----')
+    print(bmi)
+
+    -- Print body mass ratio
+    print('---- BODY MASS RATIO ----')
+    print(bmr)
+
+    -- Print ideal body mass index
+    print('---- IDEAL BODY MASS INDEX ----')
+    print(idealBmi)
+
+    -- Print body fat percentage
+    print('---- BODY FAT PERCENTAGE ----')
+    print(bodyFat)
+end
+
+return menu
